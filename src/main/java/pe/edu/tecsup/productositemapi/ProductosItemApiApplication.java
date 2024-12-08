4e6e459af4488b2d31e9a9578f3cda2ead7aed47
jgomezz <jgomezz@gmail.com>
2024-12-08 13:21:25 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/productositemapi/ProductosItemApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.productositemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosItemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosItemApiApplication.class, args);
+	}
+
+}
